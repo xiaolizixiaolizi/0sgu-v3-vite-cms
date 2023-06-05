b0488ef89c1bbb473d01b2f5e9a698c14fcd4306
xiaoliziaaa <1756329434@qq.com>
2023-06-05 22:01:43 +0800

    初始化代码

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      Check out
+      <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>
+      <div v-for="(item, index) in list" :key="index" class="aaaaaabbbbbb drewg werewrgvfd wereter retr">
+        {{ item }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const list = [1, 2, 3, 4, 4, 5, 6, 6, 7, 54, 34, 534, 3423, 32, 234]
+const a = 1
+const b = 2
+
+const fn = () => {
+  console.log('few99999999999dsjfjweojfoijrewfiojeroigjeiorjgoiejrojgoerjojo')
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+const fn = () => {
+  console.log('sss')
+}
+fn()
+
+const fn11 = () => {
+  console.log('sets')
+}
