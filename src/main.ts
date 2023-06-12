fe8799ac2d4995297732729eebec776060476887
xiaoliziaaa <1756329434@qq.com>
2023-06-12 14:17:39 +0800

    feat: 完成登录

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,7 @@ const app = createApp(App)
 app.use(router)
 app.use(ElementPlus, {
   locale: zhCn,
- 
 })
-
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(`el-icon${toLine(key)}`, component)
